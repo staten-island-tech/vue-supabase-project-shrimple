eed2aa01d8e7d814c13305eec6665e9171e83120
dell <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-05-03 14:28:42 -0400

    Update GamblingRipoff.vue

--- src/views/GamblingRipoff.vue
+++ src/views/GamblingRipoff.vue
@@ -3,43 +3,77 @@
   <div>
     <h1>hello world!</h1>
   </div>
-  <div class="container p-2">
+  <div class="container rounded">
     <div
-      class="gap-2 flex flex-row gambleContainer"
+      class="flex flex-row gambleContainer"
       ref="gambleContainer"
       :style="{ transitionDuration: `${transition}ms` }"
     >
-      <div class="gamble">{{ items[index % items.length] }}</div>
-      <div class="gamble">{{ items[(index + 1) % items.length] }}</div>
-      <div class="gamble">{{ items[(index + 2) % items.length] }}</div>
-      <div class="gamble">{{ items[(index + 3) % items.length] }}</div>
-      <div class="gamble">{{ items[(index + 4) % items.length] }}</div>
-      <div class="gamble">{{ items[(index + 5) % items.length] }}</div>
-      <div class="gamble">{{ items[(index + 6) % items.length] }}</div>
+      <div
+        class="gamble"
+        :class="(index % items.length) % 2 ? 'color' : null"
+      >
+        {{ items[index % items.length] }}
+      </div>
+      <div
+        class="gamble"
+        :class="((index + 1) % items.length) % 2 ? 'color' : null"
+      >
+        {{ items[(index + 1) % items.length] }}
+      </div>
+      <div
+        class="gamble"
+        :class="((index + 2) % items.length) % 2 ? 'color' : null"
+      >
+        {{ items[(index + 2) % items.length] }}
+      </div>
+      <div
+        class="gamble"
+        :class="((index + 3) % items.length) % 2 ? 'color' : null"
+      >
+        {{ items[(index + 3) % items.length] }}
+      </div>
+      <div
+        class="gamble"
+        :class="((index + 4) % items.length) % 2 ? 'color' : null"
+      >
+        {{ items[(index + 4) % items.length] }}
+      </div>
+      <div
+        class="gamble"
+        :class="((index + 5) % items.length) % 2 ? 'color' : null"
+      >
+        {{ items[(index + 5) % items.length] }}
+      </div>
+      <div
+        class="gamble"
+        :class="((index + 6) % items.length) % 2 ? 'color' : null"
+      >
+        {{ items[(index + 6) % items.length] }}
+      </div>
     </div>
     <span class="pointer font-mono">|</span>
   </div>
-  <code>index: {{ index }}</code>
-  <code>value: {{ items[index + 3] }}</code>
-  <code>offset: {{ offset }}</code>
   <button @click="spin">spin</button>
+  <!-- debug!!!!!! not accurate with final shift -->
+  <code>{{ items[(index + 3) % items.length] }}</code>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue";
-const items = reactive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
-const index = ref(0);
-const magic = 24 + 108;
-const offset = ref(magic);
+const items = reactive([1, 2, 3, 4, 5, "💣"]);
+const index = ref(2);
+const magic = 108;
 const gambleContainer = ref();
 const transition = ref(0);
 
 async function spin() {
-  let speen = 50;
+  let speen = Math.ceil(Math.random() * (items.length * 20 + 1)) + 299;
+  // let speen = 1;
   while (speen > 1) {
     // use desmos to get a function
     speen--;
-    await shift(700 / (speen - 0.99) ** 0.05 - 500);
+    await shift(700 / (speen - 0.99) ** 0.05 - 600);
   }
   speen--;
   await fakeShift();
@@ -48,7 +82,7 @@ async function spin() {
 async function shift(ms: number) {
   if (isNaN(ms)) ms = 600;
   if (ms < 0) ms = 10;
-  const anim = gambleContainer.value.animate([{ transform: `translateX(-${magic}px)` }, { transform: `translateX(${-magic - 108}px)` }], {
+  const anim = gambleContainer.value.animate([{ transform: `translateX(-${magic}px)` }, { transform: `translateX(${-magic - 100}px)` }], {
     duration: ms,
     iterations: 1,
   });
@@ -59,14 +93,15 @@ async function shift(ms: number) {
 }
 
 async function fakeShift() {
-  const far = Math.ceil(Math.random() * 84) + 63;
-  const anim = gambleContainer.value.animate([{ transform: `translateX(-${magic}px)`, easing: "ease-out" }, { transform: `translateX(${-magic - far}px)` }], {
+  // 50-149 (left edge to just before right edge)
+  const far = Math.ceil(Math.random() * 100) + 49;
+  const anim = gambleContainer.value.animate([{ transform: `translateX(${-magic}px)`, easing: "ease-out" }, { transform: `translateX(${-magic - far}px)` }], {
     duration: 1200,
     iterations: 1,
   });
   await anim.finished;
   transition.value = 0;
-  gambleContainer.value.style.transform = `translateX(${-magic - far + 108}px)`;
+  gambleContainer.value.style.transform = `translateX(${-magic - far + 100}px)`;
   index.value++;
   if (index.value >= items.length) index.value = 0;
   await wait(1000);
@@ -87,14 +122,15 @@ async function wait(ms: number) {
 .container {
   box-sizing: border-box;
   max-width: 500px;
-  border: 4px solid black;
+  height: 108px;
+  border: 4px solid rgb(101, 101, 101);
   overflow-x: clip;
 }
 
 .gambleContainer {
   transition-property: transform;
   transition-timing-function: ease-out;
-  transform: translateX(-24px);
+  transform: translateX(-108px);
 }
 
 .gamble {
@@ -103,13 +139,17 @@ async function wait(ms: number) {
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 2px solid black;
-  @apply rounded;
+  background-color: white;
+}
+
+.color {
+  background-color: cyan;
 }
 
 .pointer {
   position: relative;
   left: 238px;
   bottom: 10px;
+  color: red;
 }
 </style>
